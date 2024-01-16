9810238da371aceac220d138385e14eb97062607
Nathan Rydel <nathanrydel@gmail.com>
2024-01-16 12:54:35 -0700

    implemented a randomizer function

---
+++ src/random.js
@@ -0,0 +1,12 @@
+/**
+ * Return a random element from an array
+ * @param array - an array
+ * @returns  A random of element of the array
+ *  */
+
+function getRandomElement(array) {
+  let randomIndex = Math.floor(Math.random() * choices.length);
+  return array[randomIndex];
+}
+
+export default getRandomChoice;
